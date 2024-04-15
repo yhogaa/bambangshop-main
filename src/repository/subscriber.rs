f430494a425ec7b997fa2c1f2d27256338db2bed
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-04-15 16:42:25 +0700

    Implement delete function in Subscriber repository.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -12,7 +12,7 @@ pub struct SubscriberRepository;
 impl SubscriberRepository {
     pub fn add(product_type: &str, subscriber: Subscriber)->Subscriber{
         let subscriber_value = subscriber.clone();
-        if SUBSCRIBERS.get(product_type).is_none(){
+        if SUBSCRIBERS.get(product_type).is_none() {
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         };
 
@@ -22,11 +22,22 @@ impl SubscriberRepository {
     }
 
     pub fn list_all(product_type: &str)-> Vec<Subscriber>{
-        if SUBSCRIBERS.get(product_type).is_none(){
+        if SUBSCRIBERS.get(product_type).is_none() {
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         };
 
         return SUBSCRIBERS.get(product_type).unwrap().iter()
             .map(|f| f.value().clone()).collect();
+    }
+
+    pub fn delete(product_type: &str, url:&str)-> Option<Subscriber>{
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        }
+        let result = SUBSCRIBERS.get(product_type).unwrap().remove(url);
+        if !result.is_none() {
+            return Some(result.unwrap().1);
+        }
+        return None;
     }
 }
