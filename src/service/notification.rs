a51d19d8af2458af9393f21f6d1e69b3a21269d5
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-04-15 17:21:21 +0700

    Implement subscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -12,4 +12,10 @@ use super::product;
 pub struct NotificationService;
 
 impl NotificationService {
+    pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
+        let product_type_upper: String = product_type.to_uppercase();
+        let product_type_str: &str = &product_type_upper.as_str();
+        let subscriber_result : Subscriber = SubscriberRepository::add(product_type_str, subscriber);
+        return Ok(subscriber_result);
+    }
 }
